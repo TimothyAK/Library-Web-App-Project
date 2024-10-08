0ebd68804e9d5812ae846f6bff85b3b7734c0e73
TimothyAK <mothyabraham973@gmail.com>
2024-10-08 11:58:31 +0700

    First Commit Lagi ges

---
+++ src/main/java/com/sesew/librarywebappproject/LibrarywebappprojectApplication.java
@@ -0,0 +1,13 @@
+package com.sesew.librarywebappproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarywebappprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarywebappprojectApplication.class, args);
+	}
+
+}
